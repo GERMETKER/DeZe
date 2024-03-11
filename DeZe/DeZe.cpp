25f12cb511c3bc29817835ee1637bf6281871a12
GERMETKER <leha.usanov.0808@mail.ru>
2024-03-11 16:13:35 +0500

    Добавьте файлы проекта.

---
+++ DeZe/DeZe.cpp
@@ -0,0 +1,31 @@
+﻿#include <iostream>
+#include <Windows.h>
+int main()
+{
+    SetConsoleCP(1251);
+    SetConsoleOutputCP(1251);
+    /*
+    //задание1
+    int numb;
+    int sum = 0;
+    std::cout << "Введите число(до 500)\n";
+    std::cin >> numb;
+    if (numb < 500)
+    {
+        for (int i = numb; i <= 500; i++)
+        {
+            sum = sum + numb;
+        }
+        std::cout << "Сумма: " << sum+1 << "\n";
+    }
+    */
+    //задание2
+    int x, y, step;
+    std::cout << "Введите x\n";
+    std::cin >> x;
+    std::cout << "Введите y\n";
+    std::cin >> y;
+    step = x^y;
+    return 0;
+}
+
